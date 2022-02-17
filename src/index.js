82b0a083c6c3415a4915d2005c53b54bba17e7df
Aleksandr_G <shura-35@yandex.ru>
2022-02-17 19:41:37 +0300

    fix: added task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let exprArr = expr.split('**********') // сделал массив из строки. делитель - 10*.
+  // console.log (exprArr);
+  let resultExpr = []; // результирующий массив раскодированных слов
+  exprArr.forEach ((element) => { // для каждого элемента массива (строки) делим ее на подмассивы по 10 символов
+        // console.log (element);
+      let codeSymbol = element.match(/.{1,10}/g) // закодирвоанный символ
+      let decodeStr = ''; // раскодированное слово
+      codeSymbol.forEach((element) => { // для каждого закодированного символа убираем парные 00, заменяем парные 11 на тире,
+          // парные 10 - на точки
+          let morzeCode = element.replace(/00/g, '').replace(/11/g, '-').replace(/10/g, '.');
+          // console.log (morzeCode);
+          let resultSymbol = findMatch(MORSE_TABLE, morzeCode); // заменяем код морзе на букву/ цифру
+          // console.log ('after: ' + resultSymbol);
+          decodeStr = decodeStr + resultSymbol;
+      }
+      )
+      resultExpr.push(decodeStr);
+  }
+  )
+
+  // console.log (resultExpr);
+  let resultString = resultExpr.join(' ');
+  // console.log (resultString);
+  // ========= поиск совпадения элемента с ключом свойств объекта==============
+  function findMatch(object, element) {
+    for(let key in object) {
+      if (key === element){
+        element = object[key]
+        return element;
+      }
+    }
+
+  }
+  return resultString;
 }
 
 module.exports = {
